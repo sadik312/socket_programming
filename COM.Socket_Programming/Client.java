6d9745980806cb007162852410d98cc821872c3d
PrayushU <prayushudas@gmail.com>
2021-02-23 13:28:25 +0000

    file created

---
+++ COM.Socket_Programming/Client.java
@@ -0,0 +1,23 @@
+package com.socket_programming;
+
+
+public class Client{
+
+    private int static final port;
+    private String static final ip;
+
+
+    public Client(){
+
+    }
+
+    public static void setPort(int portNumber){
+        port = portNumber;
+    }
+
+    public int serPort(){
+        returen port;
+    }
+
+
+}
---
+++ COM.Socket_Programming/Server.java
@@ -0,0 +1,17 @@
+package com.socket_programming;
+
+import java.net.InetAddress;
+
+public class Server{
+
+    int static final port = 9090;
+    String final ip = InetAddress.getLocalHost();
+    Client currentCoordinator;
+
+
+
+
+    public static void main(String[] args){
+
+    }
+}
